4a55da4a10ee05365b75a4d8e33046cbc130add5
Douglas Maitelli <dougmaitelli@gmail.com>
2018-02-28 11:27:11 +0100

    Browserslist for PostCSS, Webpack 4 config, CSS minify, Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,120 @@
 "use strict";
 var webpack = require("webpack");
 var path = require("path");
-var loaders = require("./webpack.loaders");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var WebpackCleanupPlugin = require("webpack-cleanup-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || "9000";
+let loaders = [
+	{
+		test: /\.jsx?$/,
+		exclude: /(node_modules|bower_components|public\/)/,
+		loader: "babel-loader"
+	},
+	{
+		test: /\.css$/,
+		loaders: ["style-loader", "css-loader?importLoaders=1"],
+		exclude: ["node_modules"]
+	},
+	{
+		test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+		exclude: /(node_modules|bower_components)/,
+		loader: "file-loader"
+	},
+	{
+		test: /\.(woff|woff2)$/,
+		exclude: /(node_modules|bower_components)/,
+		loader: "file-loader?prefix=font/&limit=5000"
+	},
+	{
+		test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+		exclude: /(node_modules|bower_components)/,
+		loader: "file-loader?limit=10000&mimetype=application/octet-stream"
+	},
+	{
+		test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+		exclude: /(node_modules|bower_components)/,
+		loader: "file-loader?limit=10000&mimetype=image/svg+xml"
+	},
+	{
+		test: /\.gif/,
+		exclude: /(node_modules|bower_components)/,
+		loader: "file-loader?limit=10000&mimetype=image/gif"
+	},
+	{
+		test: /\.jpg/,
+		exclude: /(node_modules|bower_components)/,
+		loader: "file-loader?limit=10000&mimetype=image/jpg"
+	},
+	{
+		test: /\.png/,
+		exclude: /(node_modules|bower_components)/,
+		loader: "file-loader?limit=10000&mimetype=image/png"
+	}
+];
 
-loaders.push({
-	test: /\.scss$/,
-	loaders: ["style-loader", "css-loader?importLoaders=1", "sass-loader"],
-	exclude: ["node_modules"]
-});
+module.exports = (env, argv) => {
+	if (argv.mode === "production") {
+		loaders.push({
+			test: /\.scss$/,
+			loader: ExtractTextPlugin.extract({
+				fallback: "style-loader",
+				use: [
+					{
+						loader: "css-loader",
+						options: {
+							minimize: true,
+							sourceMap: true
+						}
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}),
+			exclude: ["node_modules"]
+		});
+	} else {
+		loaders.push({
+			test: /\.scss$/,
+			loaders: ["style-loader", "css-loader?importLoaders=1", "sass-loader"],
+			exclude: ["node_modules"]
+		});
+	}
 
-module.exports = {
-	entry: [
-		"react-hot-loader/patch",
-		"./src/index.jsx" // your app's entry point
-	],
-	output: {
-		publicPath: "/",
-		path: path.join(__dirname, "build"),
-		filename: "bundle.js"
-	},
-	resolve: {
-		extensions: [".js", ".jsx"]
-	},
-	module: {
-		rules: loaders
-	},
-	devServer: {
-		contentBase: "./build",
-		// enable HMR
-		hot: true,
-		// embed the webpack-dev-server runtime into the bundle
-		inline: true,
-		// serve index.html in place of 404 responses to allow HTML5 history
-		historyApiFallback: true,
-		port: PORT,
-		host: HOST
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new ExtractTextPlugin({
-			filename: "style.css",
-			allChunks: true
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/template.html",
-			files: {
-				css: ["style.css"],
-				js: ["bundle.js"]
-			}
-		})
-	]
+	return {
+		entry: {
+			"app.bundle": "./src/index.jsx"
+		},
+		output: {
+			publicPath: "/",
+			path: path.join(__dirname, "dist"),
+			filename: argv.mode === "production" ? "[name].[hash].js" : "[name].js"
+		},
+		resolve: {
+			extensions: [".js", ".jsx"]
+		},
+		module: {
+			rules: loaders
+		},
+		devServer: {
+			// embed the webpack-dev-server runtime into the bundle
+			inline: true,
+			// serve index.html in place of 404 responses to allow HTML5 history
+			historyApiFallback: true
+		},
+		plugins: [
+			new WebpackCleanupPlugin(),
+			new ExtractTextPlugin({
+				publicPath: "/",
+				filename: "[name].[contenthash].css"
+			}),
+			new HtmlWebpackPlugin({
+				template: "./src/template.html"
+			})
+		]
+	};
 };
